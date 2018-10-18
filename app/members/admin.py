f3a4e39b1baf7fe1e733b9fe9a4cafe1f6db42cc
Seongwon Han <seongwonhan88@gmail.com>
2018-10-18 12:29:00 +0900

    user name display

--- app/members/admin.py
+++ app/members/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'name']
+
+
+admin.site.register(User, UserAdmin)
