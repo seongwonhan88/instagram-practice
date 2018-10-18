dca04069383630b2d92e15cfdd6bb5ed6683789f
Seongwon Han <seongwonhan88@gmail.com>
2018-10-18 11:33:14 +0900

    posts, members model made

---
+++ app/members/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    img_profile = models.ImageField(
+        upload_to='user',
+        blank=True,
+    )
+    name = models.CharField(max_length=30, blank=True)
+    site = models.URLField(max_length=150, blank=True)
+    introduce = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
---
+++ app/posts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        'members.User',
+        on_delete=models.CASCADE,
+    )
+    photo = models.ImageField(upload_to='post')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        # related_name=
+    )
+    author = models.ForeignKey(
+        'members.User',
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
+
+    tags = models.ManyToManyField(
+        'HashTag',
+        blank=True,
+        # on_delete=models.SET_NULL,
+        # null=True
+    )
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
