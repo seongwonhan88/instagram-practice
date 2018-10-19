3953f8091b18bd936b299ced98f4c715ec707001
Seongwon Han <seongwonhan88@gmail.com>
2018-10-19 16:17:37 +0900

    post_create.html setup with a link

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -6,4 +6,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
+    path('create/', views.post_create, name='post-create')
 ]
--- app/posts/views.py
+++ app/posts/views.py
@@ -18,3 +18,16 @@ def post_list(request):
     }
 
     return render(request, 'posts/post_list.html', context)
+
+def post_create(request):
+    #1. posts/post_create.html
+    # form is
+    # button[type=submit]
+    # input[type=file]
+
+    #2. /posts/create/URL -> view 연결
+    #3. render사용해서 template return
+    #4. base.html nav부분에 '+add post' 텍스트 갖는 a link 추가
+    #5  {% url %} tag 사용해서 포스트 생성으로 링크 걸어주기
+
+    return render(request, 'posts/post_create.html')
