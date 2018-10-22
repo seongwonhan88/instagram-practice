3a4b4e7b94ee43b024f3b71075bb117bfec60a42
seongwonhan88 <seongwonhan88@gmail.com>
2018-10-22 11:01:17 +0900

    post_create.html form PostCreateForm setup

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from members.models import User
+from .forms import PostCreateForm
 from .models import Post
 
 
@@ -19,15 +21,27 @@ def post_list(request):
 
     return render(request, 'posts/post_list.html', context)
 
+
 def post_create(request):
-    #1. posts/post_create.html
+    # 1. posts/post_create.html
     # form is
     # button[type=submit]
     # input[type=file]
 
-    #2. /posts/create/URL -> view 연결
-    #3. render사용해서 template return
-    #4. base.html nav부분에 '+add post' 텍스트 갖는 a link 추가
-    #5  {% url %} tag 사용해서 포스트 생성으로 링크 걸어주기
+    # 2. /posts/create/URL -> view 연결
+    # 3. render사용해서 template return
+    # 4. base.html nav부분에 '+add post' 텍스트 갖는 a link 추가
+    # 5  {% url %} tag 사용해서 포스트 생성으로 링크 걸어주기
+    if request.method == 'POST':
+        Post.objects.create(author=User.objects.first(), photo=request.FILES['photo'], )
+        return redirect('posts:post-list')
+    else:
+        # get 요청의 경우 빈 Form 인스턴스를 context에 담아서 저달
+        # template에서는 'form'키로 해당 Form인스턴스 속성을 사용 가능
 
-    return render(request, 'posts/post_create.html')
+        form = PostCreateForm()
+        context = {
+            'form':form,
+        }
+
+        return render(request, 'posts/post_create.html', context)
