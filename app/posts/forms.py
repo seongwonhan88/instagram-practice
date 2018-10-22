e04433c8c520d204e44456d01dcdc331c4234dc9
seongwonhan88 <seongwonhan88@gmail.com>
2018-10-22 11:12:24 +0900

    PostCreateForm 에서 Bootstrap CSS class 사용하도록 attr 설정

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -2,4 +2,18 @@ from django import forms
 
 
 class PostCreateForm(forms.Form):
-    photo = forms.ImageField()
+    photo = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'class': 'form-control-file',
+            }
+        )
+    )
+    comment = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+            }
+        ),
+        required=False,
+    )
