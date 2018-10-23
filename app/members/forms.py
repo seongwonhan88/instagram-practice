b683defc74a1a187f8e2f7adefe591c2732f640e
seongwonhan88 <seongwonhan88@gmail.com>
2018-10-23 12:40:58 +0900

    singup form 에 field validation 구현

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForms(forms.Form):
@@ -40,3 +41,18 @@ class SignupForms(forms.Form):
             }
         )
     )
+
+    def clean_username(self):
+        # username unique?
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('that username is taken')
+        return username
+
+    def clean(self):
+        # password1 == password2 ?
+        super().clean()
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 != password2:
+            raise forms.ValidationError('password1 and password2 does not match')
